29c9274c2b9df3d85c72b58c1624d59e6748bd25
Josefina <cris@Criss-MacBook-Pro.local>
2024-04-19 13:22:22 -0300

    fix(Paginagtion): color gris si prev o next estan disabled

--- app/components/Pagination/Pagination.tsx
+++ app/components/Pagination/Pagination.tsx
@@ -11,9 +11,9 @@ const Pagination = ({ totalPages }: { totalPages: number }) => {
   return (
     <div className='flex flex-row justify-center gap-6 my-6 w-[160px] border-[1px] border-gray-800 rounded-md p-1 mx-auto'>
       <button
-        className={`text-gray-800 text-lg ${Number(page) !== 1 && 'hover:text-green-500'} ${
-          Number(page) === 1 && 'cursor-not-allowed text-gray-400'
-        } `}
+        className={`text-lg ${Number(page) !== 1 ? 'text-gray-800' : 'text-gray-400'} ${
+          Number(page) !== 1 && 'hover:text-green-500'
+        } ${Number(page) === 1 && 'cursor-not-allowed text-gray-400'} `}
         onClick={() =>
           router.push(`/?page=${Number(page) - 1}`, {
             scroll: false,
@@ -24,9 +24,9 @@ const Pagination = ({ totalPages }: { totalPages: number }) => {
       </button>
       <p className='text-gray-800 text-lg font-bold'>{page}</p>
       <button
-        className={`text-gray-800 text-lg ${Number(page) !== totalPages - 1 && 'hover:text-green-500'} ${
-          Number(page) === totalPages - 1 && 'cursor-not-allowed text-gray-400'
-        } `}
+        className={`text-lg ${Number(page) !== totalPages - 1 ? 'text-gray-800' : 'text-gray-400'} ${
+          Number(page) !== totalPages - 1 && 'hover:text-green-500'
+        } ${Number(page) === totalPages - 1 && 'cursor-not-allowed'} `}
         onClick={() =>
           router.push(`/?page=${Number(page) + 1}`, {
             scroll: false,
