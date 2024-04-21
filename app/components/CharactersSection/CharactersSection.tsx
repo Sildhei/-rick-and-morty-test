a9b6b10957653b4a717790312dbd6fc64d924d7d
Josefina <cris@Criss-MacBook-Pro.local>
2024-04-20 22:53:38 -0300

    fix(svg): fix en nombres clases

--- app/components/CharactersSection/CharactersSection.tsx
+++ app/components/CharactersSection/CharactersSection.tsx
@@ -86,9 +86,9 @@ const CharactersSection = ({
               xmlns='http://www.w3.org/2000/svg'
               fill='none'
               viewBox='0 0 24 24'
-              stroke-width='1.5'
+              strokeWidth='1.5'
               className='w-4 h-4'>
-              <path stroke-linecap='round' stroke-linejoin='round' d='M6 18 18 6M6 6l12 12' />
+              <path strokeLinecap='round' strokeLinejoin='round' d='M6 18 18 6M6 6l12 12' />
             </svg>
           </div>
         </div>
